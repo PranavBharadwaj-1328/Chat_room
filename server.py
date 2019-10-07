f90be1432ce85893cdcae0117517586fc39a4b84
PranavBharadwaj-1328 <39663257+PranavBharadwaj-1328@users.noreply.github.com>
2019-10-07 15:03:03 +0530

    Update server.py

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import os
 
 s = socket.socket()
 os.system('figlet socket is set')
-
+#enter ur ip
 host = '10.5.1.92'
 port = 1234
 
@@ -12,12 +12,17 @@ s.bind((host,port))
 print ("socket: %s"%(port))
 s.listen(5)
 print ("heard")
-
+c, addr = s.accept()
+print ('connection established with', addr)
 while True:
-    c, addr = s.accept()
-    print ('connection established with', addr)
-    a = 'Hi u are connected to PB server'
-    #a = str(a)
-    b = a.encode()
-    c.send(b)
-    c.close()
+    d = str(c)
+    #a = 'Hi %s are connected to PB server'%d
+    data = c.recv(1024).decode()
+    if(str(data) == 'bye'):
+      c, addr = s.accept()
+      print ('connection established with', addr)
+      continue
+    print ("Recieved from the above client:"+str(data))
+    data = input('->')
+    c.send(data.encode())
+
